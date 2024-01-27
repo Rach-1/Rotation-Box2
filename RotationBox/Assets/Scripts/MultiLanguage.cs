9235ddaa64921bfda2cc3a5132032f17d6577a0d
B1H1PyX <150385255+B1H1PyX@users.noreply.github.com>
2024-01-27 18:43:05 +0200

    Add spanish

--- RotationBox/Assets/Scripts/MultiLanguage.cs
+++ RotationBox/Assets/Scripts/MultiLanguage.cs
@@ -2,31 +2,50 @@ using Assets.SimpleLocalization.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings.Switch;
 
 public class MultiLanguage : MonoBehaviour
 {
     private void Awake()
     {
+        string Localization = PlayerPrefs.GetString("Localization");
         LocalizationManager.Read();
-        switch (Application.systemLanguage)
+        if (Localization == null)
         {
-            case SystemLanguage.Ukrainian:
-                LocalizationManager.Language = "Ukrainian";
-                break;
-            case SystemLanguage.Russian:
-                LocalizationManager.Language = "Ukrainian";
-                break;
-            case SystemLanguage.Belarusian:
-                LocalizationManager.Language = "Ukrainian";
-                break;
-            default:
-                LocalizationManager.Language = "English";
-                break;
+            switch (Application.systemLanguage)
+            {
+                case SystemLanguage.Ukrainian:
+                    LocalizationManager.Language = "Ukrainian";
+                    PlayerPrefs.SetString("Localization", "Ukrainian");
+                    break;
+                case SystemLanguage.Russian:
+                    LocalizationManager.Language = "Ukrainian";
+                    PlayerPrefs.SetString("Localization", "Ukrainian");
+                    break;
+                case SystemLanguage.Belarusian:
+                    LocalizationManager.Language = "Ukrainian";
+                    PlayerPrefs.SetString("Localization", "Ukrainian");
+                    break;
+                case SystemLanguage.Spanish:
+                    LocalizationManager.Language = "Spanish";
+                    PlayerPrefs.SetString("Localization", "Spanish");
+                    break;
+                default:
+                    LocalizationManager.Language = "English";
+                    PlayerPrefs.SetString("Localization", "English");
+                    break;
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetString("Localization", Localization);
+            Language(Localization);
         }
     }
 
     public void Language(string language)
     {
         LocalizationManager.Language = language;
+        PlayerPrefs.SetString("Localization", language);
     }
 }
