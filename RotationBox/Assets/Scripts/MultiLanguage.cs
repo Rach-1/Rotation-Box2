603d65760d1c38c92a72dd4b571c98470001159b
B1H1PyX <150385255+B1H1PyX@users.noreply.github.com>
2024-01-30 02:50:51 +0200

    +4 мови

--- RotationBox/Assets/Scripts/MultiLanguage.cs
+++ RotationBox/Assets/Scripts/MultiLanguage.cs
@@ -30,22 +30,46 @@ public class MultiLanguage : MonoBehaviour
                     LocalizationManager.Language = "Spanish";
                     PlayerPrefs.SetString("Localization", "Spanish");
                     break;
+                case SystemLanguage.French:
+                    LocalizationManager.Language = "French";
+                    PlayerPrefs.SetString("Localization", "French");
+                    break;
+                case SystemLanguage.Chinese:
+                    LocalizationManager.Language = "Chinese";
+                    PlayerPrefs.SetString("Localization", "Chinese");
+                    break;
+                case SystemLanguage.ChineseTraditional:
+                    LocalizationManager.Language = "Chinese";
+                    PlayerPrefs.SetString("Localization", "Chinese");
+                    break;
+                case SystemLanguage.ChineseSimplified:
+                    LocalizationManager.Language = "Chinese";
+                    PlayerPrefs.SetString("Localization", "Chinese");
+                    break;
+                case SystemLanguage.Arabic:
+                    LocalizationManager.Language = "Arabic";
+                    PlayerPrefs.SetString("Localization", "Arabic");
+                    break;
+                case SystemLanguage.Hindi:
+                    LocalizationManager.Language = "Hindi";
+                    PlayerPrefs.SetString("Localization", "Hindi");
+                    break;
                 default:
                     LocalizationManager.Language = "English";
                     PlayerPrefs.SetString("Localization", "English");
                     break;
-            }
+            } // Тут видається мова в залежності від системних налаштувань
         }
         else
         {
             PlayerPrefs.SetString("Localization", Localization);
             Language(Localization);
-        }
+        }// Тут завантажується мова з PlayerPrefs якщо це вже не перший запуск гри
     }
 
     public void Language(string language)
     {
         LocalizationManager.Language = language;
         PlayerPrefs.SetString("Localization", language);
-    }
+    } // Це для кнопок зміни мови
 }
