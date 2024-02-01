802834e3bb840780360b8bf66a6578eb8d3d2e8a
Rach-1 <donin.yar@gmail.com>
2024-02-01 22:56:47 +0300

    зміна кольору пропуску

--- RotationBox/Assets/Scripts/ChangeColorDoor.cs
+++ RotationBox/Assets/Scripts/ChangeColorDoor.cs
@@ -6,21 +6,26 @@ public class ChangeColorDoor : MonoBehaviour
 {
     [SerializeField] private Rotation rot;
     public GameObject Door;
-    public Color colorGreen;
-    public Color colorBlue;
-    public Color colorRed;
-    // Start is called before the first frame update
+    private Color ColorGreen = new Color(156f / 255f, 218f / 255f, 119f / 255f, 1f);
+    private Color ColorBlue = new Color(143f / 255f, 106f / 255f, 212f / 255f, 1f);
+    private Color ColorRed = new Color(211f / 255f, 76f / 255f, 147f / 255f, 1f);
     void Start()
     {
-    Door.GetComponent<Renderer>().material.color = ;
+        Door.GetComponent<Renderer>().material.color = ColorRed; //початковий колір
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ColorSwitch() //змінює кольори пропуску
     {
-        if(rot.IsRot)
+        switch (Door.GetComponent<Renderer>().material.color) 
         {
-
+            case Color _ when Door.GetComponent<Renderer>().material.color == ColorGreen:
+                Door.GetComponent<Renderer>().material.color = ColorBlue;
+                break;
+            case Color _ when Door.GetComponent<Renderer>().material.color == ColorBlue:
+                Door.GetComponent<Renderer>().material.color = ColorRed;
+                break;
+            case Color _ when Door.GetComponent<Renderer>().material.color == ColorRed:
+                Door.GetComponent<Renderer>().material.color = ColorGreen;
+                break;
         }
     }
 }
