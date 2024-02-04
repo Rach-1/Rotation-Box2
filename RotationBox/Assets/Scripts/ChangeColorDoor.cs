e458082b434746013718ffd8de9ecef418aca53e
Rach-1 <donin.yar@gmail.com>
2024-02-04 21:16:40 +0300

    111

--- RotationBox/Assets/Scripts/ChangeColorDoor.cs
+++ RotationBox/Assets/Scripts/ChangeColorDoor.cs
@@ -6,6 +6,8 @@ public class ChangeColorDoor : MonoBehaviour
 {
     [SerializeField] private Rotation rot;
     public GameObject Door;
+    public int ColorIs = 1; //1-червоний, 2-зелений, 3-синій
+
     private Color ColorGreen = new Color(156f / 255f, 218f / 255f, 119f / 255f, 1f);
     private Color ColorBlue = new Color(143f / 255f, 106f / 255f, 212f / 255f, 1f);
     private Color ColorRed = new Color(211f / 255f, 76f / 255f, 147f / 255f, 1f);
@@ -19,12 +21,63 @@ public class ChangeColorDoor : MonoBehaviour
         {
             case Color _ when Door.GetComponent<Renderer>().material.color == ColorGreen:
                 Door.GetComponent<Renderer>().material.color = ColorBlue;
+                ColorIs = 3;
                 break;
             case Color _ when Door.GetComponent<Renderer>().material.color == ColorBlue:
                 Door.GetComponent<Renderer>().material.color = ColorRed;
+                ColorIs = 1;
                 break;
             case Color _ when Door.GetComponent<Renderer>().material.color == ColorRed:
                 Door.GetComponent<Renderer>().material.color = ColorGreen;
+                ColorIs = 2;
+                break;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        switch(ColorIs)
+        {
+            case 1:
+                if (collision.gameObject.tag == "Red" || collision.gameObject.tag == "RGB")
+                {
+                    GetComponent<BoxCollider2D>().enabled = false;
+                }
+                break;
+            case 2:
+                if (collision.gameObject.tag == "Green" || collision.gameObject.tag == "RGB")
+                {
+                    GetComponent<BoxCollider2D>().enabled = false;
+                }
+                break;
+            case 3:
+                if (collision.gameObject.tag == "Blue" || collision.gameObject.tag == "RGB")
+                {
+                    GetComponent<BoxCollider2D>().enabled = false;
+                }
+                break;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        switch (ColorIs)
+        {
+            case 1:
+                if (collision.gameObject.tag == "Red" || collision.gameObject.tag == "RGB")
+                {
+                    GetComponent<BoxCollider2D>().enabled = true;
+                }
+                break;
+            case 2:
+                if (collision.gameObject.tag == "Green" || collision.gameObject.tag == "RGB")
+                {
+                    GetComponent<BoxCollider2D>().enabled = true;
+                }
+                break;
+            case 3:
+                if (collision.gameObject.tag == "Blue" || collision.gameObject.tag == "RGB")
+                {
+                    GetComponent<BoxCollider2D>().enabled = true;
+                }
                 break;
         }
     }
