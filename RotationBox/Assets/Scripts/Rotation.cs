d9557a5afa91262d8165c6fdf90cae5caef12674
Rach-1 <donin.yar@gmail.com>
2024-11-23 22:27:04 +0300

    Додав плавне обертання лабіринту

--- RotationBox/Assets/Scripts/Rotation.cs
+++ RotationBox/Assets/Scripts/Rotation.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Rotation : MonoBehaviour
 {
@@ -16,6 +17,8 @@ public class Rotation : MonoBehaviour
     public Box box3;
     public Box box4;
     private int Grad = 0;
+    public float rotationSpeed = 90f; // Швидкість обертання в градусах за секунду
+    private bool isRotating = false;
 
     public ChangeColorDoor door;
 
@@ -32,23 +35,112 @@ public class Rotation : MonoBehaviour
         GetComponent<AudioSource>().PlayOneShot(rote);
         //door.ColorSwitch();
     }
+    //public void Right()
+    //{
+    //    if (box.isStopped && box1.isStopped && box2.isStopped && box3.isStopped && box4.isStopped)
+    //    {
+    //        Grad -= 90;
+    //        transform.rotation = Quaternion.Euler(0, 0, Grad);
+    //        PlaySound();
+    //    }
+    //}
+    //public void Left()
+    //{
+    //    if (box.isStopped && box1.isStopped && box2.isStopped && box3.isStopped && box4.isStopped)
+    //    {
+    //        Grad += 90;
+    //        transform.rotation = Quaternion.Euler(0, 0, Grad);
+    //        PlaySound();
+    //    }
+    //}
     public void Right()
     {
-        if (box.isStopped && box1.isStopped && box2.isStopped && box3.isStopped && box4.isStopped)
+        if (!isRotating)
         {
-            Grad -= 90;
-            transform.rotation = Quaternion.Euler(0, 0, Grad);
-            PlaySound();
+            StartCoroutine(RotateR90Degrees());
         }
     }
     public void Left()
     {
-        if (box.isStopped && box1.isStopped && box2.isStopped && box3.isStopped && box4.isStopped)
+        if (!isRotating)
         {
-            Grad += 90;
-            transform.rotation = Quaternion.Euler(0, 0, Grad);
-            PlaySound();
+            StartCoroutine(RotateL90Degrees());
         }
+    }
+
+    private IEnumerator RotateR90Degrees()
+    {
+        isRotating = true;
+        PlaySound();
+
+        // Збереження дочірніх об'єктів і їх Rigidbody
+        Rigidbody[] childRigidbodies = GetComponentsInChildren<Rigidbody>();
+
+        // Вимкнення гравітації у дочірніх об'єктів
+        foreach (var rb in childRigidbodies)
+        {
+            rb.useGravity = false;
+        }
+
+        float targetRotation = transform.eulerAngles.z - 90;
+        float initialRotation = transform.eulerAngles.z;
+        float rotationProgress = 0f;
+
+        while (rotationProgress < 1f)
+        {
+            rotationProgress += Time.deltaTime * rotationSpeed / 90;
+            float currentRotation = Mathf.Lerp(initialRotation, targetRotation, rotationProgress);
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, currentRotation);
+            yield return null;
+        }
+
+        // Гарантуємо точність фінальної позиції
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, targetRotation);
+
+        // Увімкнення гравітації у дочірніх об'єктів
+        foreach (var rb in childRigidbodies)
+        {
+            rb.useGravity = true;
+        }
+
+        isRotating = false;
+    }
+    private IEnumerator RotateL90Degrees()
+    {
+        isRotating = true;
+        PlaySound();
+
+        // Збереження дочірніх об'єктів і їх Rigidbody
+        Rigidbody[] childRigidbodies = GetComponentsInChildren<Rigidbody>();
+
+        // Вимкнення гравітації у дочірніх об'єктів
+        foreach (var rb in childRigidbodies)
+        {
+            rb.useGravity = false;
+        }
+
+        float targetRotation = transform.eulerAngles.z + 90;
+        float initialRotation = transform.eulerAngles.z;
+        float rotationProgress = 0f;
+
+        while (rotationProgress < 1f)
+        {
+            rotationProgress += Time.deltaTime * rotationSpeed / 90;
+            float currentRotation = Mathf.Lerp(initialRotation, targetRotation, rotationProgress);
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, currentRotation);
+            yield return null;
+        }
+
+        // Гарантуємо точність фінальної позиції
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, targetRotation);
+
+        // Увімкнення гравітації у дочірніх об'єктів
+        foreach (var rb in childRigidbodies)
+        {
+            rb.useGravity = true;
+        }
+
+        isRotating = false;
     }
 }
 
